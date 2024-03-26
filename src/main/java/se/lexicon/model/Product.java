91155a59221d603fc97194d4e3681a515952f20c
Elnaz <azizi.elnaz66@gmail.com>
2024-03-26 09:40:44 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/model/Product.java
@@ -0,0 +1,36 @@
+package se.lexicon.model;
+
+public abstract class Product extends Object {
+    private static int sequencer = 0;
+    private int id;
+    private final double price;
+    private String productName;
+
+    public Product(double price, String productName) {
+        this.price = price;
+        this.productName = productName;
+        this.id= getNextID();
+
+    }
+
+    private static int getNextId() {
+        return ++sequencer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        if (productName == null) throw new IllegalArgumentException("productName is null");
+        this.productName = productName;
+    }
+}
